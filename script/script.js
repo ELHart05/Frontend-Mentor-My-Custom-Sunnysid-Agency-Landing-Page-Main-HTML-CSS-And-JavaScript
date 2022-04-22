44bea6cb29badd48f4dad526710cfb9b9a684d45
ALLAOUA Okba <96151694+ELHart05@users.noreply.github.com>
2022-04-22 20:52:27 +0100

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,11 +1,16 @@
 const icon = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav-bar');
 const navmob = document.querySelector('.nav-mob');
 const arrow = document.querySelector('.arrow');
 const triangle = document.querySelector('.triangle');
 
-icon.onclick = function nav_mob_shower() {
+//functionto hide the arrow back the nav mob
+function arrowhide() {
     arrow.classList.toggle('arrow-hide');
+}
+
+//appear the nav mob with animation and also hiding the arrow with timout of 0.3s
+icon.onclick = function nav_mob_shower() {
+    setTimeout(arrowhide,300);
     triangle.classList.toggle('triangle-add');
     navmob.classList.toggle('navmob-edit');
 }
